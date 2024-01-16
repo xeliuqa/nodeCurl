1a7ad8398891399fc1752717742b2e6d9a9c3b29
Jonh <info@xeliuqa.app>
2024-01-16 19:48:52 +0000

    error handling

--- NodeCurl.ps1
+++ NodeCurl.ps1
@@ -1,133 +1,140 @@
 <#  -----------------------------------------------------------------------------------------------
-.VERSION 1.15
+.VERSION 1.16
 .AUTHOR
 .PROJECTURI https://github.com/xeliuqa/nodeCurl
 
 Get grpcurl here: https://github.com/fullstorydev/grpcurl/releases
 	-------------------------------------------------------------------------------------------- #>
 
-$host.UI.RawUI.WindowTitle = "Node Curl"
-$grpcurl = $PSScriptRoot + "\grpcurl.exe"
-Write-Host -ForegroundColor Green "
-	----------------------------------------
-	        Welcome to nodeCurl
-	----------------------------------------"
-function main {
-
-    while ($selection -ne 'Q') {
-        Clear-Host
-        #Main Menu
-        $ip = Read-Host -Prompt "Please select IP Address (default 127.0.0.1)"
-        if ([string]::IsNullOrWhiteSpace($ip)) { $ip = "127.0.0.1" }
-        Write-Host $ip -ForegroundColor Green
-
-        $port1 = Read-Host -Prompt "Please select Port 1 (default 9092)"
-        if ([string]::IsNullOrWhiteSpace($port1)) { $port1 = "9092" }
-        Write-Host $port1 -ForegroundColor Green
-
-        $port2 = Read-Host -Prompt "Please select Port 2 (default 9093)"
-        if ([string]::IsNullOrWhiteSpace($port2)) { $port2 = "9093" }
-        Write-Host $port2 -ForegroundColor Green
-
-        Write-Host -ForegroundColor Yellow "
-    ------------------------------
-    |         Main Menu          |
-    ------------------------------"
-        $MenuOptions = @'
-	"Press '0' - Restart (Change Ports)"
-	"Press '1' - Events Stream"
-	"Press '2' - Check Node status"
-	"Press '3' - Node Version"
-	"Press '4' - Smesh Service"
-	"Press '5' - Highest ATX" (Might take up to 5 minutes)
-	"Press '6' - Node ID"
-	"Press '7' - PoST Status"
-	"Press '8' - Check if banned"
-	"Press 'Q' - Quit."
-'@
-
-        "`n$MenuOptions"
-
-        :selectionLoop while (($selection = Read-Host -Prompt "`nSelect a option") -ne 'Q') {
+    $host.UI.RawUI.WindowTitle = "Node Curl"
+    $grpcurl = $PSScriptRoot + "\grpcurl.exe"
+    Write-Host -ForegroundColor Green "
+            ----------------------------------------
+                    Welcome to nodeCurl
+            ----------------------------------------"
+    function main {
+        
+        while ($selection -ne 'Q') {
             Clear-Host
-
+            #Main Menu
+            $ip = Read-Host -Prompt "Please select IP Address (default 127.0.0.1)"
+            if ([string]::IsNullOrWhiteSpace($ip)) { $ip = "127.0.0.1" }
+            Write-Host $ip -ForegroundColor Green
+        
+            $port1 = Read-Host -Prompt "Please select Port 1 (default 9092)"
+            if ([string]::IsNullOrWhiteSpace($port1)) { $port1 = "9092" }
+            Write-Host $port1 -ForegroundColor Green
+        
+            $port2 = Read-Host -Prompt "Please select Port 2 (default 9093)"
+            if ([string]::IsNullOrWhiteSpace($port2)) { $port2 = "9093" }
+            Write-Host $port2 -ForegroundColor Green
+        
+            Write-Host -ForegroundColor Yellow "
+            ------------------------------
+            |         Main Menu          |
+            ------------------------------"
+            $MenuOptions = @'
+            "Press '0' - Restart (Change Ports)"
+            "Press '1' - Events Stream"
+            "Press '2' - Check Node status"
+            "Press '3' - Node Version"
+            "Press '4' - Smesh Service"
+            "Press '5' - Highest ATX" (Might take up to 5 minutes)
+            "Press '6' - Node ID"
+            "Press '7' - PoST Status"
+            "Press '8' - Check if banned"
+            "Press 'Q' - Quit."
+'@
+        
             "`n$MenuOptions"
-
-            switch ( $selection ) {
-                0 { break selectionLoop }
-                1 {
-                    write-host "`n"
-                    write-host "Please wait ..." 
-                    $job = Start-Job -ScriptBlock {
-                        param($ip, $port2, $grpcurl)
-                        & $grpcurl "-plaintext" "$($ip):$($port2)" "spacemesh.v1.AdminService.EventsStream"
-                    } -ArgumentList $ip, $port2, $grpcurl
-                    Wait-Job -Timeout 3 -Job $job
-                    Receive-Job -Job $job
-                    Remove-Job -Job $job -Force
-                }
-                #1 { ./grpcurl.exe -plaintext "$($ip):$($port2)" "spacemesh.v1.AdminService.EventsStream"}
-                2 { & $grpcurl -plaintext "$($ip):$($port1)" "spacemesh.v1.NodeService.Status" }
-                3 { & $grpcurl -plaintext "$($ip):$($port1)" "spacemesh.v1.NodeService.Version" }
-                4 { & $grpcurl -plaintext "$($ip):$($port2)" "spacemesh.v1.SmesherService.IsSmeshing" }
-                5 {
-                    write-host "`n" 
-                    write-host "Please wait ..." 
-                    & $grpcurl -plaintext "$($ip):$($port1)" "spacemesh.v1.ActivationService.Highest"
-                }
-                #6 { ./grpcurl.exe -plaintext "$($ip):$($port2)" "spacemesh.v1.SmesherService.SmesherID"}
-                6 {
-                    Write-Host "Node ID: " -ForegroundColor Cyan
-                    Write-Host "Hex    = " -ForegroundColor Yellow -NoNewline; $publicKey = ((Invoke-Expression ("$($grpcurl) --plaintext -max-time 3 $($ip):$($port2) spacemesh.v1.SmesherService.SmesherID")) | ConvertFrom-Json).publicKey 2>$null
-                    B64_to_Hex -id2convert $publicKey 
-                    Write-Host "Base64 = " -ForegroundColor Yellow -NoNewline; $publicKey
-                }
-                7 { & $grpcurl -plaintext "$($ip):$($port2)" "spacemesh.v1.SmesherService.PostSetupStatus" }
-                8 {
-                    write-host "`n" 
-                    write-host "Please wait ..."
-                    $publicKey = ((Invoke-Expression ("$($grpcurl) --plaintext -max-time 5 $($ip):$($port2) spacemesh.v1.SmesherService.SmesherID")) | ConvertFrom-Json).publicKey 2>$null
-                    if ($null -ne $publicKey) {
-                        $publicKey = (B64_to_Hex -id2convert $publicKey)
-                        $publicKeylow = $publicKey.ToLower()
+        
+            :selectionLoop while (($selection = Read-Host -Prompt "`nSelect a option") -ne 'Q') {
+                Clear-Host
+        
+                "`n$MenuOptions"
+        
+                switch ( $selection ) {
+                    0 { break selectionLoop }
+                    1 {
+                        write-host "`n"
+                        write-host "Please wait ..." 
                         $job = Start-Job -ScriptBlock {
-                            param($ip, $port1, $grpcurl)
-                            & $grpcurl "-plaintext" "$($ip):$($port1)" "spacemesh.v1.MeshService.MalfeasanceStream"
-                        } -ArgumentList $ip, $port1, $grpcurl
+                            param($ip, $port2, $grpcurl)
+                            & $grpcurl "-plaintext" "$($ip):$($port2)" "spacemesh.v1.AdminService.EventsStream"
+                        } -ArgumentList $ip, $port2, $grpcurl
                         Wait-Job -Timeout 3 -Job $job
-                        $response = Receive-Job -Job $job
+                        Receive-Job -Job $job
                         Remove-Job -Job $job -Force
-                        if ($response -match $publicKeylow) {
-                            write-host "`n"
-                            write-host "Your node ID is: " -NoNewline
-                            Write-Host $publicKey -ForegroundColor Yellow
-                            write-host "The node has been banned" -ForegroundColor Yellow
+                    }
+                    #1 { ./grpcurl.exe -plaintext "$($ip):$($port2)" "spacemesh.v1.AdminService.EventsStream"}
+                    2 { & $grpcurl -plaintext "$($ip):$($port1)" "spacemesh.v1.NodeService.Status" }
+                    3 { & $grpcurl -plaintext "$($ip):$($port1)" "spacemesh.v1.NodeService.Version" }
+                    4 { & $grpcurl -plaintext "$($ip):$($port2)" "spacemesh.v1.SmesherService.IsSmeshing" }
+                    5 {
+                        write-host "`n" 
+                        write-host "Please wait ..." 
+                        & $grpcurl -plaintext "$($ip):$($port1)" "spacemesh.v1.ActivationService.Highest"
+                    }
+                    #6 { ./grpcurl.exe -plaintext "$($ip):$($port2)" "spacemesh.v1.SmesherService.SmesherID"}
+                    6 {
+                        Write-Host "Node ID: " -ForegroundColor Cyan
+                        Write-Host "Hex    = " -ForegroundColor Yellow -NoNewline; $publicKey = ((Invoke-Expression ("$($grpcurl) --plaintext -max-time 3 $($ip):$($port2) spacemesh.v1.SmesherService.SmesherID")) | ConvertFrom-Json).publicKey 2>$null
+                        B64_to_Hex -id2convert $publicKey 
+                        Write-Host "Base64 = " -ForegroundColor Yellow -NoNewline; $publicKey
+                    }
+                    7 { & $grpcurl -plaintext "$($ip):$($port2)" "spacemesh.v1.SmesherService.PostSetupStatus" }
+                    8 {
+                        write-host "`n" 
+                        write-host "Please wait ..."
+                        $publicKey = ((Invoke-Expression ("$($grpcurl) --plaintext -max-time 5 $($ip):$($port2) spacemesh.v1.SmesherService.SmesherID")) | ConvertFrom-Json).publicKey 2>$null
+                        if ($null -eq $publicKey) {
+                            write-host "Something went wrong" -ForegroundColor Red
+                            write-host "Could not retreive Malfeasance List" -ForegroundColor Red
+                            write-host "Please use option 0 to restart script and choose correct port" -ForegroundColor Red
                         }
                         else {
-                            write-host "`n"
-                            write-host "Your node ID is:" -NoNewline
-                            Write-Host $publicKey -ForegroundColor Yellow
-                            write-host "It looks alright"
+                            if ($null -ne $publicKey) {
+                                $publicKey = (B64_to_Hex -id2convert $publicKey)
+                                $publicKeylow = $publicKey.ToLower()
+                                $job = Start-Job -ScriptBlock {
+                                    param($ip, $port1, $grpcurl)
+                                    & $grpcurl "-plaintext" "$($ip):$($port1)" "spacemesh.v1.MeshService.MalfeasanceStream"
+                                } -ArgumentList $ip, $port1, $grpcurl
+                                Wait-Job -Timeout 3 -Job $job
+                                $response = Receive-Job -Job $job
+                                Remove-Job -Job $job -Force
+                                if ($response -match $publicKeylow) {
+                                    write-host "`n"
+                                    write-host "Your node ID is: " -NoNewline
+                                    Write-Host $publicKey -ForegroundColor Yellow
+                                    write-host "The node has been banned" -ForegroundColor Yellow
+                                }
+                                else {
+                                    write-host "`n"
+                                    write-host "Your node ID is:" -NoNewline
+                                    Write-Host $publicKey -ForegroundColor Yellow
+                                    write-host "It looks alright"
+                                } 
+                            }
+                            else {
+                                write-host "`n"
+                                write-host "The node is offline or you're trying to probe Smapp"
+                            }
                         }
                     }
-                    else {
-                        write-host "`n"
-                        write-host "The node is offline or you're trying to probe Smapp"
-                    }
+                    Q { 'Quit' }
+                    default { 'Invalid entry' }
                 }
-                Q { 'Quit' }
-                default { 'Invalid entry' }
             }
         }
     }
-}
-
-function B64_to_Hex {
-    param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [string]$id2convert
-    )
-    [System.BitConverter]::ToString([System.Convert]::FromBase64String($id2convert)).Replace("-", "")
-}
-
-main
+        
+    function B64_to_Hex {
+        param (
+            [Parameter(Position = 0, Mandatory = $true)]
+            [string]$id2convert
+        )
+        [System.BitConverter]::ToString([System.Convert]::FromBase64String($id2convert)).Replace("-", "")
+    }
+        
+    main
